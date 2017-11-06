143f31e10fe34667dc0e6d64366ac82e243bc5b4
钟孝帮 <442203968@qq.com>
2017-11-06 15:24:22 +0800

    书籍更新器

--- Book/BookRefresh.cs
+++ Book/BookRefresh.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Book.Model;
+using Book.Data;
+using Book.Common;
+using System.Net.Http;
 
 namespace Book
 {
@@ -11,6 +15,23 @@ namespace Book
     /// </summary>
     public class BookRefresh
     {
-        
+        private UserInfo userinfo;
+        private List<BookInfo> books;
+        public BookRefresh()
+        {
+            userinfo = UserInfoHelper.GetUserInfo();
+            UserBookListData ubl = new UserBookListData();
+            books = ubl.GetBooks(userinfo.ID);
+        }
+
+        public async void Start()
+        {
+            HttpClient client = new HttpClient();
+            foreach (var item in books)
+            {
+
+                List<ChapterInfo> chapters = new ChapterInfoData(item).GetChapterInfos();
+            }
+        }
     }
 }
