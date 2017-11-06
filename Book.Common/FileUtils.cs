a2c642a1a6b92a137a8818490a21e30630fada32
钟孝帮 <442203968@qq.com>
2017-11-06 11:15:32 +0800

    增加一点注释

--- Book.Common/FileUtils.cs
+++ Book.Common/FileUtils.cs
@@ -12,10 +12,22 @@ namespace Book.Common
     /// </summary>
     public class FileUtils
     {
+        /// <summary>
+        /// 打开一个文件，使用指定的编码读取文件的所有行，然后关闭该文件。
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="encoding"></param>
+        /// <returns></returns>
         public static string ReadAllText(string path, Encoding encoding)
         {
             return File.ReadAllText(path, encoding);
         }
+        /// <summary>
+        /// 创建一个新文件，在其中写入指定的字符串，然后关闭文件。如果目标文件已存在，则覆盖该文件。
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="contents"></param>
+        /// <param name="encoding"></param>
         public static void WriteAllText(string path, string contents, Encoding encoding)
         {
             string directoryName = Path.GetDirectoryName(path);
